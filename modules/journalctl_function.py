19c830c11985c48af53aed2f02950bc6380dcd73
BanditHagen <simonottosson6@gmail.com>
2025-12-20 13:16:18 +0100

    Add journalctl_function to branch modules/journalctl_function

---
+++ modules/journalctl_function.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+import subprocess
+import time
+import sys
+import colors
+
+# Dictonary, each keyword is an alert label and each label is a list of patterns to look for to generate warnings.
+keywords = {
+    "SUDO Attempt Failed" : ["incorrect password attempt", "not in sudoers", "user not in sudoers"],
+    "Failed Login" : ["failed password for", "sshd.*authentication failure", "failed publickey", "invalid user"],
+    "SUDO Command Executed" : ["pam_unix(sudo:session): session opened for user root"],
+    "User Change Event" : ["useradd", "userdel", "usermod", "password changed for"],
+    "ROOT SSH Login" : ["Accepted password for root", "Accepted publickey for root"],
+    "Dangerous Command" : ["command=/bin/rm -rf", "command=/bin/dd", "command=/sbin/shutdown", "command=/usr/sbin/reboot"]
+    } 
+
+# List of words to ignore(reduce noise).
+ignore_keywords = ["systemd", "cron", "session closed", "session opened for user root by"]
+
+
+def journalctl_section(keywords, ignore_keywords):
+    try:
+        process = subprocess.Popen(["journalctl", "-f", "-n", "0"], stdout = subprocess.PIPE, stderr = subprocess.PIPE, text = True)
+            # stdout stores the output from "journalctl -f".
+            # stderr stores any error messages.
+            # "-f" means follow, -> adds new log entries in real-time.
+            # "-n 0" means show 0 old lines -> only show NEW log entries from start of program.
+            # text = True makes output to string format instead of binaries.
+        
+        count = 0   # Count variable initiated.
+
+        # Main loop to process each entry piped out to stdout by journalclt.
+        for line in process.stdout:
+            line = line.strip() # Removes whitespace before and after.
+
+            ########################  Ignore Keywords part  ########################
+            # Check for matches in ignore keywords list.
+            should_ignore = False
+            for ignore_word in ignore_keywords: # Loop through each ignore keyword.
+                if ignore_word.lower() in line.lower(): # if the ignore keyword is found in the line <- stdout.
+                    should_ignore = True    
+                    break  # if True match is found, exit loop early.
+        
+            if should_ignore: 
+                continue  # Skip to next line if ignore keyword was found.
+
+            ########################  Find Keywords part  ########################
+            
+            should_alert = False
+            for alert_label, patterns in keywords.items(): # Loop through each category in the keywords dictionary.
+                if should_alert:    # If matched stop going through the alert labels.
+                    break
+
+                for pattern in patterns:
+                    if pattern.lower() in line.lower(): # If pattern is in line <- stdout do this:
+                        count += 1
+
+                        if "command=" in line.lower():  # To extract commands if any executed. 
+                            command_start = line.lower().find("command=")    # Find "command=" in line <- stdout.
+                            command = line[command_start + 8:].split(";")[0].strip()   # Command = 8 characters, find the word after "command=" until ";"
+                            print(f"{count} [{alert_label}] COMMAND: {command}") # Prints which command executed.
+                        else:
+                            print(f"{count} [{alert_label}]")   # If no command executed print
+                        
+                        print(f"Full Event: {line}")    # Finally print this:
+                        print("-" * 100)
+                        should_alert = True 
+                        break   # Stop looking for patterns
+        
+                
+    except KeyboardInterrupt:
+        print('\033[96mRealtime monitoring service has been stopped.\033[0m')
+        process.terminate()
+    finally:
+        process.terminate()
+
+
+if __name__ == "__main__":  # make it possible to test separately for function.
+    journalctl_section(keywords, ignore_keywords)
