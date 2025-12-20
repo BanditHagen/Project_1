a39c8df7ce8b532163a7711d53c128486261dea7
BanditHagen <simonottosson6@gmail.com>
2025-12-20 13:04:42 +0100

    Add functional and visual updates to startup_menu

--- modules/startup_menu.py
+++ modules/startup_menu.py
@@ -7,11 +7,13 @@ import colors
 
 def main():
     # Initial startup messages.
+    print("\033c")  # Clears terminal output.
     print(colors.cyan("=" * 39))
     print(colors.cyan("<<Booting realtime monitoring servies>>"))
     print(colors.cyan("=" * 39))
+    time.sleep(2)
+    print("\033c")  #   flushes the output in terminal
     time.sleep(1)
-
     ###############################     Small system Checkup    ######################################
 
     #   System information + check if on right OS.
@@ -27,28 +29,33 @@ def main():
     print(colors.blue("=" * 26))
     print(colors.blue("<<System check completed>>"))
     print(colors.blue("=" * 26))
-
+    time.sleep(2)
 ##########################  Menu for various choice output    #########################################
-    time.sleep(1)
     while True:
-        menu_controller = input("\n[Q]-quit\n[I]-information\n[C]-continue\n\n//:")
+        print("\033c")  # For each lap terminal output is cleared.
+        menu_controller = input(colors.white("\n[Q]-quit\n[I]-information\n[C]-continue\n\n//:"))
         if menu_controller.lower() == "q":
+            print("\033c")  # Clears terminal output
             print(colors.cyan("Closing program..."))
             sys.exit()
             break
         elif menu_controller.lower() == "i":
+            print("\033c")  # Clears terminal output
             print("=" * 29)
             print("Information about the program")
             print("=" * 29)
+            input("Press Enter to continue")
         elif menu_controller.lower() == "c":
+            print("\033c")  # Clears terminal output.
             print(colors.blue("=" * 32))
             print(colors.blue("<<Monitoring service initiated>>"))
             print(colors.blue("=" * 32))
             break
-    
+        else:   # Wrong input displays error message. 
+            print(colors.red("<<Input failure>> \nplease chose from menu."))
+            time.sleep(1)
 
 
-
-
+#   Makes module runnable by itself for testing.
 if __name__ == "__main__":
     main()
