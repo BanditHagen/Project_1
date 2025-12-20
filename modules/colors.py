360d270799e1c7d7ce290ea8ed6a6b891b97c69a
BanditHagen <simonottosson6@gmail.com>
2025-12-20 11:33:00 +0100

    Add more colors to module along with option of using highlights

--- modules/colors.py
+++ modules/colors.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+# On call return a colored string. 
 def red(text):
     return f"\033[91m{text}\033[0m"
 
@@ -18,10 +18,53 @@ def magenta(text):
 def cyan(text):
     return f"\033[96m{text}\033[0m"
 
+def white(text):
+    return f"\033[97m{text}\033[0m"
+
+def black(text):
+    return f"\033[90m{text}\033[0m"
+
+# On call return highligt on a string
+def hlred(text):
+    return f"\033[41m{text}\033[0m"
+
+def hlyellow(text):
+    return f"\033[43m{text}\033[0m"
+
+def hlcyan(text):
+    return f"\033[46m{text}\033[0m"
+
+def hlmagenta(text):
+    return f"\033[45m{text}\033[0m"
+
+def hlblue(text):
+    return f"\033[44m{text}\033[0m"
+
+def hlwhite(text):
+    return f"\033[47m{text}\033[0m"
+
+def hlblack(text):
+    return f"\033[40m{text}\033[0m"
+
+def hlgreen(text):
+    return f"\033[42m{text}\033[0m"
 
 if __name__ == "__main__":  # makes script runnable standalone for testing.
+    # Colored string test/display
     print(red("testing red"))
     print(yellow("testing yellow"))
     print(cyan("testing cyan"))
     print(magenta("testing magenta"))
     print(blue("testing blue"))
+    print(green("testing green"))
+    print(white("testing white"))
+    print(black("testing black"))
+    # Highlight test/display
+    print(hlred("testing red highlight"))
+    print(hlyellow("testing yellow highlight"))
+    print(hlcyan("testing cyan highlight"))
+    print(hlmagenta("testing magenta highlight"))
+    print(hlblue("testing blue highlight"))
+    print(hlgreen("testing green highlight"))
+    print(hlwhite("testing white highlight"))
+    print(hlblack("testing black highlight"))
