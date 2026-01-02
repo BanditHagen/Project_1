181a2e555c13d828fe9e58056358ce127f2a919a
BanditHagen <simonottosson6@gmail.com>
2026-01-02 13:02:34 +0100

    Check added to control if run with sudo privilege to main.py

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ from modules.startup_menu import startup, display_menu
 from modules.journalctl_function import journalctl_function, keywords, ignore_keywords, keyword_colors
 from modules import colors
 import sys
+import os
+import time
 #   Main entry point for Project_1
 #   Realtime system monitor using "journalctl" on Linux.
 def main():
-    #   Launch startup + menu.
-    try:
-        startup()
-        if display_menu():  # If returning True(user input "c") start journalctl_function.
-            journalctl_function(keywords, ignore_keywords, keyword_colors)
-    except KeyboardInterrupt:   # "Ctl + C" interrupts program and displays message and exits.
-        print(colors.cyan("Realtime monitoring service has been stopped."))
+    if os.getuid() == 0:
+        #   Launch startup + menu.
+        try:
+            startup()
+            if display_menu():  # If returning True(user input "c") start journalctl_function.
+                journalctl_function(keywords, ignore_keywords, keyword_colors)
+        except KeyboardInterrupt:   # "Ctl + C" interrupts program and displays message and exits.
+            print(colors.cyan("Realtime monitoring service has been stopped."))
+            sys.exit()
+    else:
+        print(colors.red("WARNING, sudo privilege required. Rerun with correct credentials."))
+        time.sleep(2)
         sys.exit()
 
 if __name__ == "__main__":
